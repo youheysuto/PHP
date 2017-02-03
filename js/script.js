ac89affd8cc6f4347464a7b7b9535c9b82de69fe
須藤洋平 <sutouyouhei@sudouyouheinoMacBook-Pro.local>
2017-02-03 20:36:55 +0900

    動画組み込み　20170203

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+
+function vidFade() {
+  console.log(vid);
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+  {
+    //only functional if "loop" is removed 
+    vid.pause();
+    // to capture IE10
+    vidFade();
+  }); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+});
+
